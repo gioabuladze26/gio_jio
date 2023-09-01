3e1648e02de0ed7bb45c7f6be063df283f5ebe51
giorgi abuladze <143558324+gioabuladze26@users.noreply.github.com>
2023-09-02 00:04:29 +0200

    Add files via upload

---
+++ group 102/day 1/lesson1.py
@@ -0,0 +1,44 @@
+name = "giorgi"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# giorgi არის ცვლადის მნიშვნელი
+surname = "abuladze"
+
+print (name)
+
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+name = ("giorgi") #ეს არის str (string) ტიპის ცვლადი
+age = 30 # ეს არის int (integer) მთელი რიცხვი
+height = 175,3 # ეს არის floot ტიპის ცვლადი
+
+#boolean (bool) ტიპის ცვლადი
+#knows_programing = True    #True ან False   
+
+#knows_programing = # snakecase weris standartuli stili
+#knowsprograming = # javaskript camelcase
+
+print (name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+# print(name + age) #False
+
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname)
+#print(type(height))
+#print(type(knows_programing))
+
+
+
+name = ("giorgi")
+surname= ("abuladze")
+hight = (175)
+age = (30)
+
+print(name + " " + surname + " " + str(age) + " " + str(hight))
+
